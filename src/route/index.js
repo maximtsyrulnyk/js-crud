25ae28809aeb7b26ab459026dd445401e4cfdb23
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-04-12 13:46:20 +0300

    have troubes with create function

--- src/route/index.js
+++ src/route/index.js
@@ -5,21 +5,63 @@ const router = express.Router()
 
 // ================================================================
 
+class Product {
+  static #list = []
+  constructor(name, price, description) {
+    this.name = name
+    this.price = price
+    this.description = description
+  }
+  static add = (product) => {
+    this.#list.push(product)
+  }
+  static getList = () => {
+    return this.#list
+  }
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
+  const list = Product.getList();
+
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/product-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const {name, price, description} = req.body
+
+  const product = new Product(name, price, description);
+  Product.add(product)
+  console.log(Product.getList());
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('product-create', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'product-create',
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
