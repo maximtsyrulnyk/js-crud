c6a776e873b992395f8854e17a0ca0a6d018d6ac
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-04-19 21:15:41 +0300

    have some troubles in project

--- src/route/index.js
+++ src/route/index.js
@@ -34,6 +34,48 @@ router.get('/', function (req, res) {
 })
 
 // ================================================================
+router.get('/purchase-index', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('purchase-index', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-index',
+
+    data: {
+      img: 'https://picsum.photos/200/300',
+      title: `Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/`,
+      description: `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ`,
+      category: [
+        {id: 1, text: 'Готовий до відправки'}, 
+        {id: 2, text: 'Топ продажів'}, 
+      ],
+      price: 27000, 
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+
+router.get('/purchase-product', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('purchase-product', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-product',
+
+    data: {
+      img: 'https://picsum.photos/200/300',
+      title: `Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/`,
+      description: `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ`,
+      category: [
+        {id: 1, text: 'Готовий до відправки'}, 
+        {id: 2, text: 'Топ продажів'}, 
+      ],
+      price: 27000, 
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
