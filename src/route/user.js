21e93b55c25af622d6089a0ce7471f85c0444df9
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-04-19 20:34:36 +0300

    have some troubles in project

--- src/route/user.js
+++ src/route/user.js
@@ -6,28 +6,23 @@ const router = express.Router()
 // ================================================================
 
 class Purchase {
-
+ 
 }
+
+// ================================================================
 
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
+
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('purchase-index', {
+  res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'purchase-index',
+    style: 'index',
 
     data: {
-      img: 'https://picsum.photos/200/300',
-      title: `Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/`,
-      description: `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ`,
-      category: [
-        {id: 1, text: 'Готовий до відправки'}, 
-        {id: 2, text: 'Топ продажів'}, 
-      ],
-      price: 27000, 
     },
   })
   // ↑↑ сюди вводимо JSON дані
